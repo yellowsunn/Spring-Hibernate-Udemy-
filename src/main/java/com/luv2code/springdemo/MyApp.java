4e07357cc256cdd924af4c1d9b6c0053092474bd
yellowsunn <chohankook95@gmail.com>
2020-03-23 17:00:19 +0900

    Rough Prototype Part 1

---
+++ src/main/java/com/luv2code/springdemo/MyApp.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+public class MyApp {
+    public static void main(String[] args) {
+        // create the object
+        Coach theCoach = new BaseballCoach();
+
+        // use the object
+        System.out.println(theCoach.getDailyWorkout());
+    }
+}
