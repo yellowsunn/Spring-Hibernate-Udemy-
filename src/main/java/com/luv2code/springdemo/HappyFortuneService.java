c16eab576723ca86c9c348f6ed46a11c6ee1b639
yellowsunn <chohankook95@gmail.com>
2020-03-24 20:11:34 +0900

    코드 초기화

--- src/main/java/com/luv2code/springdemo/HappyFortuneService.java
+++
@@ -1,7 +0,0 @@
-package com.luv2code.springdemo;
-
-public class HappyFortuneService implements  FortuneService {
-    public String getFortune() {
-        return "Today is your lucky day!";
-    }
-}
