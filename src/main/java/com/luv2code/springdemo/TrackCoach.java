071590b625c1c9d12bf8a34714b564ea31bad8d5
yellowsunn <chohankook95@gmail.com>
2020-03-24 19:13:30 +0900

    빈 라이프 사이클 코드 작성

--- src/main/java/com/luv2code/springdemo/TrackCoach.java
+++ src/main/java/com/luv2code/springdemo/TrackCoach.java
@@ -17,4 +17,14 @@ public class TrackCoach implements Coach {
     public String getDailyFortune() {
         return "Just Do It: " + fortuneService.getFortune();
     }
+
+    // add an init method
+    public void doMyStartUpStuff() {
+        System.out.println("TrackCoach: inside method doMyStartUpStuff");
+    }
+
+    // add a destroy method
+    public void doMyCleanUpStuffYoYo() {
+        System.out.println("TrackCoach: inside method doMyCleanUpStuffYoYo");
+    }
 }
