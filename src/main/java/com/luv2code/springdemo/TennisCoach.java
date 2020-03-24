ddca52cdeab5946b08660971d367dbaeb0f16008
yellowsunn <chohankook95@gmail.com>
2020-03-24 22:48:11 +0900

    @Autowired 를 이용한 setter 주입

--- src/main/java/com/luv2code/springdemo/TennisCoach.java
+++ src/main/java/com/luv2code/springdemo/TennisCoach.java
@@ -8,10 +8,22 @@ public class TennisCoach implements Coach {
 
     private FortuneService fortuneService;
 
+    // define a default constructor
+    public TennisCoach() {
+        System.out.println(">> TennisCoach: inside default constructor");
+    }
+
+    // define a setter method
     @Autowired
-    public TennisCoach(FortuneService fortuneService) {
+    public void setFortuneService(FortuneService fortuneService) {
+        System.out.println(">> TennisCoach: inside setFortuneService() method");
         this.fortuneService = fortuneService;
     }
+
+//    @Autowired
+//    public TennisCoach(FortuneService fortuneService) {
+//        this.fortuneService = fortuneService;
+//    }
 
     @Override
     public String getDailyWorkOut() {
