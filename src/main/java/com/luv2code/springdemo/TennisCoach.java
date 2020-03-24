4b5c48a8b0b7a387b9fe389a5bd11d6cee2e99c4
yellowsunn <chohankook95@gmail.com>
2020-03-24 23:19:58 +0900

    @Autowired 를 이용한 필드 주입(자바 리플렉션에 의해 주입되는것)

--- src/main/java/com/luv2code/springdemo/TennisCoach.java
+++ src/main/java/com/luv2code/springdemo/TennisCoach.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+    @Autowired
     private FortuneService fortuneService;
 
     // define a default constructor
@@ -13,11 +14,11 @@ public class TennisCoach implements Coach {
         System.out.println(">> TennisCoach: inside default constructor");
     }
 
-    @Autowired
-    public void domSomeCrazyStuff(FortuneService fortuneService) {
-        System.out.println(">> TennisCoach: inside domSomeCrazyStuff() method");
-        this.fortuneService = fortuneService;
-    }
+//    @Autowired
+//    public void domSomeCrazyStuff(FortuneService fortuneService) {
+//        System.out.println(">> TennisCoach: inside domSomeCrazyStuff() method");
+//        this.fortuneService = fortuneService;
+//    }
 
 //    @Autowired
 //    public TennisCoach(FortuneService fortuneService) {
