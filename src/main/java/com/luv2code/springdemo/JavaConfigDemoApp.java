df86c13072552c029702e21190c277dc1e0cac1f
yellowsunn <chohankook95@gmail.com>
2020-03-31 20:03:30 +0900

    @PropertySource 로 프로퍼티 파일에 있는 값 주입하기
    - 빈을 등록할 때 반환형이 Coach 인데도 실제 빈을 불러올때 원래 클래스인 SwimCoach 로도 불러올 수 있다??

--- src/main/java/com/luv2code/springdemo/JavaConfigDemoApp.java
+++ src/main/java/com/luv2code/springdemo/JavaConfigDemoApp.java
@@ -9,13 +9,17 @@ public class JavaConfigDemoApp {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(SportConfig.class);
         // get the bean from spring container
-        Coach theCoach = context.getBean("swimCoach", Coach.class);
+        SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
 
         // call a method on the bean
         System.out.println(theCoach.getDailyWorkOut());
 
         // call method to get the daily fortune
         System.out.println(theCoach.getDailyFortune());
+
+        // call our new swim coach methods ... has the props values injected
+        System.out.println("email: " + theCoach.getEmail());
+        System.out.println("team: " + theCoach.getTeam());
 
         // close the context
         context.close();
--- src/main/java/com/luv2code/springdemo/SwimCoach.java
+++ src/main/java/com/luv2code/springdemo/SwimCoach.java
@@ -1,9 +1,15 @@
 package com.luv2code.springdemo;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
 
 public class SwimCoach implements Coach {
     private FortuneService fortuneService;
+
+    @Value("${foo.email}")
+    private String email;
+
+    @Value("${foo.team}")
+    private String team;
 
     public SwimCoach(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
@@ -17,5 +23,12 @@ public class SwimCoach implements Coach {
     @Override
     public String getDailyFortune() {
         return fortuneService.getFortune();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public String getTeam() {
+        return team;
     }
 }
