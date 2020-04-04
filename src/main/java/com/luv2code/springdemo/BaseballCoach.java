c9a7c112c973d1f6e06370cbcb31e1f21ebff98e
yellowsunn <chohankook95@gmail.com>
2020-04-04 19:42:22 +0900

    필요없는 파일 삭제

--- src/main/java/com/luv2code/springdemo/BaseballCoach.java
+++
@@ -1,7 +0,0 @@
-package com.luv2code.springdemo;
-
-public class BaseballCoach implements Coach{
-    public String getDailyWorkout() {
-        return "Spend 30 minutes on batting practice";
-    }
-}
