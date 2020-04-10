d0d0fd15bd664f043e7c6ef7abb7233cb1aa3e7c
yellowsunn <chohankook95@gmail.com>
2020-04-10 21:25:07 +0900

    <form:radiobuttons> 태그 추가 - hashMap 객체 이용

--- src/main/java/com/yellowsunn/mvc/Student.java
+++ src/main/java/com/yellowsunn/mvc/Student.java
@@ -1,15 +1,33 @@
 package com.yellowsunn.mvc;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
+import java.util.LinkedHashMap;
+
 @Getter @Setter
 public class Student {
     private String firstName;
     private String lastName;
 
     private String country;
+
     private String favoriteLanguage;
+    // 좋아하는 언어 옵션들
+    @Setter(AccessLevel.NONE)
+    private LinkedHashMap<String, String> favoriteLanguageOptions;
+
+    public Student() {
+        favoriteLanguageOptions = new LinkedHashMap<>();
+//        favoriteLanguageOptions.put("Java", "Java");
+//        favoriteLanguageOptions.put("C#", "C#");
+//        favoriteLanguageOptions.put("PHP", "PHP");
+//        favoriteLanguageOptions.put("Ruby", "Ruby");
+        favoriteLanguageOptions.put("Java", "자바");
+        favoriteLanguageOptions.put("C#", "C#");
+        favoriteLanguageOptions.put("PHP", "PHP");
+        favoriteLanguageOptions.put("Ruby", "루비");
+    }
 }
--- src/main/webapp/WEB-INF/view/student-form.jsp
+++ src/main/webapp/WEB-INF/view/student-form.jsp
@@ -21,10 +21,7 @@
         </form:select> <br/><br/>
 
         Favorite Language :
-        Java<form:radiobutton path="favoriteLanguage" value="Java"/>
-        C#<form:radiobutton path="favoriteLanguage" value="C#"/>
-        PHP<form:radiobutton path="favoriteLanguage" value="PHP"/>
-        Ruby<form:radiobutton path="favoriteLanguage" value="Ruby"/> <br/><br/>
+        <form:radiobuttons path="favoriteLanguage" items="${student.favoriteLanguageOptions}"/> <br/><br/>
         <input type="submit" value="Submit"/>
     </form:form>
 </body>
