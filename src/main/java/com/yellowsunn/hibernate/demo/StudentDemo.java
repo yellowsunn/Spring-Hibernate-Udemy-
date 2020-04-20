8ce9afff123463b55e6a8061d25d75e0c414cf31
DESKTOP-M7J4JVT\choil <chohankook95@gmail.com>
2020-04-20 23:33:55 +0900

    쿼리문으로 원하는 조건의 데이터 Read 하기 2

--- src/main/java/com/yellowsunn/hibernate/demo/StudentDemo.java
+++ src/main/java/com/yellowsunn/hibernate/demo/StudentDemo.java
@@ -37,10 +37,18 @@ public class StudentDemo {
             displayStudents(theStudents);
 
             // query students: lastName= 'Doe'
-            theStudents = session.createQuery("from Student s where s.lastName='Doe'").getResultList();
-
-            // display the students
             System.out.println("\n\nStudents who have last name of Doe");
+            theStudents = session.createQuery("from Student s where s.lastName='Doe'").getResultList();
+            displayStudents(theStudents);
+
+            // query students: lastName='Doe' OR firstName='Daffy'
+            System.out.println("\n\nStudents who have last name of Doe OR first name Daffy");
+            theStudents = session.createQuery("from Student s where s.lastName='Doe' or s.firstName='Daffy'").getResultList();
+            displayStudents(theStudents);
+
+            // query students where email LIKE '%gmail.com'
+            System.out.println("\n\nStudents who email ends with gmail.com");
+            theStudents = session.createQuery("from Student s where s.email LIKE '%gmail.com'").getResultList();
             displayStudents(theStudents);
 
             // commit the transaction
